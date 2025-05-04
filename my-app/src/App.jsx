d6d65fcb660857cbd05ba9e72bdc606524c993fd
adityajain256 <jainaditya25522@gmail.com>
2025-05-04 08:06:37 +0530

    q

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+    <h1 id="h1">Created first react app by vite| aditya jain</h1>
+    <button id="cl">click</button>
+    </>
+  )
+}
+
+
+
+export default App
